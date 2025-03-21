6553daf970d79b0133745b5171b2c642e568850e
Ziheng <2563373919@qq.com>
2025-03-21 20:31:23 +0800

    ADD 2

--- MultiplyThreeNums.py
+++ MultiplyThreeNums.py
@@ -1,6 +1,5 @@
-# MultiplyThreeNums.py
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 num3 = float(input("Enter the third number: "))
 result = num1 * num2 * num3
-print("The product of the three numbers is:", result)
+print("Product of the three numbers is:", result)
